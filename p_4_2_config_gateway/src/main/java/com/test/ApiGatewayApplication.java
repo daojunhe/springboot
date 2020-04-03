52c09067f7b20945152609be926affa217d25aaa
Emrys He <emrys.he@GRC.MP>
2020-04-03 10:38:50 +0800

    zuul动态网关，通过统一配置管理zuul配置文件

--- p_4_2_config_gateway/src/main/java/com/test/ApiGatewayApplication.java
+++ p_4_2_config_gateway/src/main/java/com/test/ApiGatewayApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 //@ComponentScan(basePackages = "com.zpc.gateway.filter")
 public class ApiGatewayApplication {
 
@@ -17,11 +19,4 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-    @RefreshScope
-    @ConfigurationProperties("zuul")
-    public ZuulProperties zuulProperties(){
-        ZuulProperties properties = new ZuulProperties();
-        System.out.println("properties:"+properties);
-        return properties ;
-    }
 }
